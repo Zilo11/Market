31d1d24892d115ea4414b1dfc1e26803c00fbfd7
Nyuydine Bill leynyuy <billleynyuy@gmail.com>
2023-08-13 22:38:38 +0100

    sqlite

--- zilo/settings.py
+++ zilo/settings.py
@@ -93,8 +93,15 @@ WSGI_APPLICATION = 'zilo.wsgi.application'
 #         'PORT': '6030',
 #     }
 # }
+# DATABASES = {
+#     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
+
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
