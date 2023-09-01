eb21afe4a2a02909789fefef698cda653f15f833
Zilo11 <137594239+Zilo11@users.noreply.github.com>
2023-09-02 00:04:31 +0100

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 
 from item.models import Category, Item
+from django.contrib.auth.forms import UserCreationForm
+
 
 from .forms import SignupForm
-from django.contrib.auth import logout
+from django.contrib.auth import logout,login
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -29,18 +31,15 @@ def termsAndConditions(request):
 
 def signup(request):
     if request.method == 'POST':
-        form = SignupForm(request.POST)
-
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-
-            return redirect('/termsAndConditions/')
+            user = form.save()
+            login(request, user)  # Automatically log in the user
+            return redirect('/termsAndConditions/')  
     else:
-        form = SignupForm()
-
-    return render(request, 'core/signup.html', {
-        'form': form
-    })
+        form = UserCreationForm()
+    
+    return render(request, 'core/signup.html', {'form': form})
     
 def sign_out(request):
     logout(request)
