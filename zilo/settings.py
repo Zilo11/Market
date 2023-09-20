fd1481f62565a73452ee0f158d3dab5acf9b4eea
NyuydineBill <billleynyuy@gmail.com>
2023-09-20 23:18:27 +0100

    Added a search bar at home page This is because someone can post the same product many times And this will stress the user more

--- zilo/settings.py
+++ zilo/settings.py
@@ -98,16 +98,12 @@ WSGI_APPLICATION = 'zilo.wsgi.application'
 #     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
