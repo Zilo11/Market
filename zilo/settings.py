7d48a219569a40a822edd95e0ba6b7c7730f7e23
NyuydineBill <billleynyuy@gmail.com>
2023-09-21 20:58:47 +0100

    made logo decoration

--- zilo/settings.py
+++ zilo/settings.py
@@ -98,15 +98,15 @@ WSGI_APPLICATION = 'zilo.wsgi.application'
 #     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+ }
+# DATABASES = {
+#     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
 
 # Password validation
