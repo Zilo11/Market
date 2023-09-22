2846b6a08afe4119c31a920b9b84d495aa7cfd2e
kongnyuy livingston <kongnyuylivingston2@gmail.com>
2023-09-22 05:45:55 +0100

    allowed admin to approved items and also also deleted users items

--- core/views.py
+++ core/views.py
@@ -8,7 +8,7 @@ from .forms import SignupForm
 from django.contrib.auth import logout,login
 
 def index(request):
-    items = Item.objects.filter(is_sold=False)[0:6]
+    items = Item.objects.filter(is_approved=True, is_sold=False)[0:12]
     categories = Category.objects.all()
 
     return render(request, 'core/index.html', {
