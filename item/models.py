740e447b323f96e83a10f400a9b704a1d7fe6e94
NyuydineBill <billleynyuy@gmail.com>
2023-09-23 16:39:32 +0100

    Added changes to location, phone nummber modified the contact form

--- item/models.py
+++ item/models.py
@@ -18,7 +18,7 @@ class Item(models.Model):
     category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    location = models.CharField(max_length=100, default=timezone.now)
+    location = models.CharField(max_length=100, default="Bamenda")
     price = models.FloatField()
     image = models.ImageField(upload_to='item_images', blank=True, null=True)
     is_sold = models.BooleanField(default=False)
