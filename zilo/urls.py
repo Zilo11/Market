62fcfa13d687694e374150b1e112aef0aa6c99d1
NyuydineBill <billleynyuy@gmail.com>
2023-10-08 23:16:19 +0100

    lkslks

--- zilo/urls.py
+++ zilo/urls.py
@@ -12,8 +12,6 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('inbox/', include('conversation.urls')),
     path('notification/', include('notifications_app.urls')),
-    path('firebase-messaging-sw.js',showFirebaseJS,name="show_firebase_js"),
-    path('send/',send,name="show_firebase_js"),
     path('reset_password/',auth_views.PasswordResetView.as_view(template_name='core/passwordReset.html'),name='reset_password'),
     path('password_reset_done/',auth_views.PasswordResetDoneView.as_view(template_name='core/passwordResetSent.html'), name ="password_reset_done"),
     path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='core/passwordResetForm.html'), name="password_reset_confirm"),
