0fbb14d2c4921010333280fb9e1a2ee80b295532
kongnyuy livingston <kongnyuylivingston2@gmail.com>
2023-10-10 04:13:21 +0100

    fixed the details page

--- zilo/settings.py
+++ zilo/settings.py
@@ -125,12 +125,12 @@ DATABASES = {
 }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
- }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+#  }
 
 
 
